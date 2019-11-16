6f09c850a3c2081625565cf2c7a8d815532aff6f
antonS <so4usertrue@gmail.com>
2019-11-16 14:01:15 +0300

    скуCreate preloader sprites and run games

--- js/game.js
+++ js/game.js
@@ -1,6 +1,45 @@
 const game = {
+    canvas : null,
+    ctx    : null,
+    sprites: {
+        background: null,
+        cell      : null,
+    },
+
+    //старт игры
     start() {
-        console.log('!!')
+        this.canvas = document.getElementById('mycanvas');
+        this.ctx    = this.canvas.getContext("2d");
+        this.preload((key) => {
+            this.run();
+        })
+    },
+
+    //предзагрузка спрайтов
+    preload(callback) {
+        let loaded        = 0;
+        const required    = 2;
+        const onAssetLoad = () => {
+            loaded += 1;
+
+            if (loaded >= required) {
+                callback();
+            }
+        };
+
+        Object.keys(this.sprites).forEach((key) => {
+            this.sprites[key]     = new Image();
+            this.sprites[key].src = `img/${key}.png`;
+            this.sprites[key].addEventListener('load', onAssetLoad);
+        });
+    },
+
+    //запуск игры
+    run() {
+        window.requestAnimationFrame(() => {
+            game.ctx.drawImage(this.sprites.background, 0, 0);
+            game.ctx.drawImage(this.sprites.cell, 320, 180);
+        });
     }
 };
 
